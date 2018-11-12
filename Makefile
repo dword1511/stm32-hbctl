40cafbaf685a3540eb2ff6cb76d2ac2b9e882aff
Chi Zhang <dword@czhang-desktop.ece.wisc.edu>
2018-11-11 18:00:50 -0800

    working port

--- Makefile
+++ Makefile
@@ -1,23 +1,140 @@
-LIBOPENCM3  = libopencm3/lib/libopencm3_stm32f0.a
+PROGRAM    := stm32-hbctl
+SRCS       := hbctl.c
+CROSS      ?= arm-none-eabi-
 
-include common.mk
+###############################################################################
 
-# libopencm3 enables FPU on CM4 by default
-ARCH_FLAGS  = -DSTM32F0 -mthumb -mcpu=cortex-m0 -msoft-float
-LDSCRIPT    = libopencm3/lib/stm32/f0/stm32f05xz8.ld
-OPENCM3_MK  = lib/stm32/f0
+# Clear implicit rules
+.SUFFIXES:
 
-.PHONY: flash debug erase
+AR         := $(CROSS)ar
+CC         := $(CROSS)gcc
+LD         := $(CROSS)ld
+OBJCOPY    := $(CROSS)objcopy
+OBJDUMP    := $(CROSS)objdump
+SIZE       := $(CROSS)size
+NM         := $(CROSS)nm
+GDB        := gdb-multiarch
+
+ELF        := $(PROGRAM).elf
+BIN        := $(PROGRAM).bin
+HEX        := $(PROGRAM).hex
+MAP        := $(PROGRAM).map
+DMP        := $(PROGRAM).out
+
+
+TOPDIR     := $(shell pwd)
+DEPDIR     := $(TOPDIR)/.dep
+OBJS       := $(SRCS:.c=.o)
+
+
+# Debugging (code has to be bullet-proof since it can cause physical damage!)
+# NOTE: Most unsigned constants is missing the U
+CFLAGS     += -Wall -Wextra -Wdouble-promotion -gdwarf-4 -g3
+#CFLAGS     += -Wconversion -Wno-sign-conversion
+# Optimizations
+# NOTE: without optimization everything will fail... Computational power is marginal.
+CFLAGS     += -O3 -fbranch-target-load-optimize -fipa-pta -frename-registers -fgcse-sm -fgcse-las -fsplit-loops -fstdarg-opt
+# Use these for debugging-friendly binary
+#CFLAGS     += -O0
+CFLAGS     += -Og
+# Disabling aggressive loop optimizations since it does not work for loops longer than certain iterations
+#CFLAGS     += -fno-aggressive-loop-optimizations
+# Aggressive optimizations (unstable or causes huge binaries)
+#CFLAGS     += -funroll-loops -fbranch-target-load-optimize2
+# Includes
+CFLAGS     += -Ilibopencm3/include/ -I$(TOPDIR)
+# General for MCU
+CFLAGS     += -fno-common -ffunction-sections -fdata-sections -fsingle-precision-constant -ffast-math -flto --specs=nano.specs
+
+# Generate dependency information
+CFLAGS     += -MT $@ -MMD -MP -MF $(DEPDIR)/$*.Td
+$(shell mkdir -p $(DEPDIR) >/dev/null)
+PRECOMPILE  = mkdir -p $(dir $(DEPDIR)/$*.Td)
+POSTCOMPILE = mv -f $(DEPDIR)/$*.Td $(DEPDIR)/$*.d && touch $@
+
+# Architecture-dependent
+LDSCRIPT   := libopencm3/lib/stm32/f0/stm32f04xz6.ld
+OPENCM3_MK := lib/stm32/f0
+LIBOPENCM3 := libopencm3/lib/libopencm3_stm32f0.a
+FP_FLAGS   := -msoft-float
+CFLAGS     += -DSTM32F0 -mthumb -mcpu=cortex-m0 $(FP_FLAGS)
+# LDPATH is required for libopencm3's ld scripts to work.
+LDPATH     := libopencm3/lib/
+# NOTE: the rule will ensure CFLAGS are added during linking
+LDFLAGS    += -nostdlib -L$(LDPATH) -T$(LDSCRIPT) -Wl,-Map -Wl,$(MAP) -Wl,--gc-sections -Wl,--relax
+LDLIBS     += $(LIBOPENCM3) -lc -lgcc
+
+
+# Pass these to libopencm3's Makefile
+PREFIX     := $(patsubst %-,%,$(CROSS))
+V          := 0
+export FP_FLAGS CFLAGS PREFIX V
+
+
+default: $(BIN) $(HEX) $(DMP) size
+
+$(ELF): $(LDSCRIPT) $(OBJS) $(LIBOPENCM3) Makefile
+	$(CC) -o $@ $(CFLAGS) $(LDFLAGS) $(OBJS) $(LDLIBS)
+
+$(DMP): $(ELF)
+	$(OBJDUMP) -d $< > $@
+
+%.hex: %.elf
+	$(OBJCOPY) -S -O ihex   $< $@
+
+%.bin: %.elf
+	$(OBJCOPY) -S -O binary $< $@
+
+%.o: %.c $(LIBOPENCM3) Makefile
+	$(PRECOMPILE)
+	$(CC) $(CFLAGS) -c $< -o $@
+	$(POSTCOMPILE)
+
+# NOTE: libopencm3's Makefile is unaware of top-level Makefile changes, so force remake
+# ld script also obtained here. However, has to touch it to prevent unnecessary remakes
+$(LIBOPENCM3) $(LDSCRIPT): Makefile
+	git submodule update --init
+	make -B -C libopencm3 $(OPENCM3_MK)
+	touch $(LDSCRIPT)
+
+
+.PHONY: default clean distclean size symbols symbols_bss flash erase debug monitor check
+
+clean:
+	rm -f $(OBJS) $(ELF) $(HEX) $(BIN) $(MAP) $(DMP)
+	rm -rf $(DEPDIR)
+
+distclean: clean
+	make -C libopencm3 clean
+	rm -f *.o *~ *.swp *.hex
+
+# These targets want clean terminal output
+size: $(ELF)
+	@echo ""
+	@$(SIZE) $<
+	@echo ""
+
+symbols: $(ELF)
+	@$(NM) --demangle --size-sort -S $< | grep -v ' [bB] '
+
+symbols_bss: $(ELF)
+	@$(NM) --demangle --size-sort -S $< | grep ' [bB] '
 
 flash: $(HEX)
-	@killall st-util || echo -n
+	@killall st-util || echo
 	@st-flash --reset --format ihex write $<
 
-debug: $(BIN) $(ELF)
-	@killall st-util || echo -n
-	@$(XTERM) st-util &
-	@$(GDB) $(ELF) -q -ex 'target extended-remote localhost:4242'
-
 erase:
-	@killall st-util || echo -n
-	@st-flash --reset erase
+	@killall st-util || echo
+	@st-flash erase
+
+debug: $(ELF) flash
+	@killall st-util || echo
+	@setsid st-util &
+	@-$(GDB) $< -q -ex 'target extended-remote localhost:4242'
+
+# Dependencies
+$(DEPDIR)/%.d:
+.PRECIOUS: $(DEPDIR)/%.d
+include $(wildcard $(patsubst %,$(DEPDIR)/%.d,$(basename $(SRCS))))
--- hbctl.c
+++ hbctl.c
@@ -3,160 +3,43 @@
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/adc.h>
 
-#define GPIO_BANK_BUT GPIOA
-#define GPIO_BUT      GPIO0
-#define GPIO_BANK_LED GPIOC
-#define GPIO_LED1     GPIO8
-#define GPIO_LED2     GPIO9
+
+#define GPIO_PORT_BUT GPIOB
+#define GPIO_PIN_BUT  GPIO8
+#define GPIO_PORT_LED GPIOA
+#define GPIO_PIN_LED1 GPIO6
+#define GPIO_PIN_LED2 GPIO7
+
+#define GPIO_PORT_CS  GPIOA
+#define GPIO_PIN_CSP  GPIO5
+#define ADC_CH_CSP    5
+#define GPIO_PIN_CSN  GPIO4
+#define ADC_CH_CSN    4
+#define GPIO_PORT_VIN GPIOB
+#define GPIO_PIN_VIN  GPIO1
+#define ADC_CH_VIN    9
+
+#define ADC_R_MOHM    100
+#define ADC_VDDA_MV   3300
+#define ADC_DIGITS    (1 << 12)
+#define ADC_CLOCK     12000000
+#define ADC_CONV_CYCS 241   /* 239.5 + 1.5 */
+#define ADC_SAMP_T_MS 50
+#define ADC_OVERSAMP  (((ADC_CLOCK / 1000 / 2) * ADC_SAMP_T_MS) / ADC_CONV_CYCS) /* Each measurement samples 2 channels */
+
+#define GPIO_PORT_PWM GPIOA
+#define GPIO_AF_PWM   GPIO_AF2
+#define GPIO_PIN_SYNC GPIO0
+#define GPIO_PIN_HI   GPIO1
+#define GPIO_PIN_LO   GPIO2
+#define PWM_TIM       TIM2
+#define PWM_TIM_RCC   RCC_TIM2
+#define PWM_TIM_RST   RST_TIM2
+#define PWM_OC_HI     TIM_OC2
+#define PWM_OC_LO     TIM_OC3
 
 
-static uint8_t  channel_array_1[16] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-
-static void setup_adc(void) {
-  rcc_periph_clock_enable(RCC_ADC1);
-
-  /* Configure PA2 and PA4 for analog input */
-
-  gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
-
-  /* Configure PA1, PA3 and PA5 as guard (ground) */
-  gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_100MHZ, GPIO0 | GPIO2);
-  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0 | GPIO2);
-  gpio_clear(GPIOA, GPIO0 | GPIO2);
-
-  adc_power_off(ADC1);
-  adc_set_clk_source(ADC1, ADC_CLKSOURCE_PCLK_DIV4); /* Set ADC clock to 12 MHz */
-  adc_calibrate(ADC1);
-  adc_set_regular_sequence(ADC1, 1, channel_array_1);
-  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_239DOT5); /* (239.5 + 1.5 ~= 50 ksps) */
-  adc_set_resolution(ADC1, ADC_RESOLUTION_12BIT);
-  adc_set_right_aligned(ADC1);
-  ADC_CFGR1(ADC1) |= ADC_CFGR1_AUTDLY; /* Do not start conversion until DR is read. Avoids race conditions. */
-  adc_power_on(ADC1);
-  //while (!adc_is_power_on(ADC1));
-
-  adc_set_continuous_conversion_mode(ADC1);
-  adc_start_conversion_regular(ADC1);
-}
-
-#define ADC_OVERSAMPLE 20000 /* ~ 400ms */
-
-static uint32_t read_adc(void) {
-  /* Oversample act as FIR LPF */
-  uint32_t adc_acc = 0;
-  int radc_i;
-
-  adc_read_regular(ADC1);
-  for (radc_i = 0; radc_i < ADC_OVERSAMPLE; radc_i ++) {
-    while (!adc_eoc(ADC1));
-    adc_acc += adc_read_regular(ADC1);
-  }
-
-  return adc_acc;
-}
-
-static void swcap_setup_gpio(void) {
-  /* These should be PA9 and PA10 */
-  /* Setup TIM1 channel 2 (channel 1 pin occupied by MCO, channel 4 is single-ended) */
-  gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO9);
-  gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO0);
-  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
-  gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO0);
-  gpio_set_af(GPIOA, GPIO_AF2, GPIO9);
-  gpio_set_af(GPIOB, GPIO_AF2, GPIO0);
-
-  /* In case of EMI (ADC setup already done) */
-  gpio_port_config_lock(GPIOA, GPIO9);
-  gpio_port_config_lock(GPIOB, GPIO0);
-}
-
-static void swcap_setup_timer(uint16_t period, uint16_t duty, uint16_t deadtime) {
-  rcc_periph_clock_enable(RCC_TIM1);
-  rcc_periph_reset_pulse(RST_TIM1);
-
-  timer_disable_oc_output(TIM1, TIM_OC1);
-  timer_disable_oc_output(TIM1, TIM_OC1N);
-  timer_disable_oc_output(TIM1, TIM_OC2);
-  timer_disable_oc_output(TIM1, TIM_OC2N);
-  timer_disable_oc_output(TIM1, TIM_OC3);
-  timer_disable_oc_output(TIM1, TIM_OC3N);
-  timer_disable_oc_output(TIM1, TIM_OC4);
-
-  timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-  timer_set_prescaler(TIM1, 0);
-  timer_continuous_mode(TIM1);
-
-  timer_set_period(TIM1, period);
-  if (deadtime != 0) {
-    timer_set_deadtime(TIM1, deadtime);
-  }
-
-  timer_set_break_lock(TIM1, TIM_BDTR_LOCK_OFF);
-  timer_disable_break(TIM1);
-
-  timer_disable_oc_clear(TIM1, TIM_OC2);
-  timer_enable_oc_preload(TIM1, TIM_OC2);
-  timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_PWM1);
-  timer_set_oc_value(TIM1, TIM_OC2, duty);
-
-  timer_enable_break_main_output(TIM1); /* Required for advanced timer to work */
-  timer_enable_oc_output(TIM1, TIM_OC2);
-  timer_enable_oc_output(TIM1, TIM_OC2N);
-  timer_enable_counter(TIM1);
-}
-
-/* TIM1 as Switched Capacitor Clock Source */
-/* Use channel 2 or 3 to obtain complementary output without clashing with MCO */
-static void swcap_setup(uint32_t freq, uint8_t duty, uint16_t deadtime) {
-  uint16_t period = rcc_apb2_frequency / freq - 1;
-  uint16_t ocv = (((uint32_t)duty) * ((uint32_t)period)) / 100 + 1;
-  /* Period / duty calculation is OK */
-
-  /* GPIOA, AFIO should have been enabled by this point */
-  swcap_setup_gpio();
-  swcap_setup_timer(period, ocv, deadtime);
-}
-
-static void button_setup(void) {
-  gpio_mode_setup(GPIO_BANK_BUT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_BUT);
-
-  /* In case of EMI */
-  gpio_port_config_lock(GPIO_BANK_BUT, GPIO_BUT);
-}
-
-static void button_wait(void) {
-  uint32_t i = 0;
-
-  while (!gpio_get(GPIO_BANK_BUT, GPIO_BUT)) {
-    i ++;
-    if (((i % 500000) == 1) || ((i % 500000) == 10000)) {
-      gpio_toggle(GPIO_BANK_LED, GPIO_LED1 | GPIO_LED2);
-    }
-  }
-
-  /* De-bounce */
-  for (i = 0; i < 100000; i ++) {
-    if (gpio_get(GPIO_BANK_BUT, GPIO_BUT)) {
-      i = 0;
-    }
-  }
-}
-
-static void led_setup(void) {
-  gpio_set_output_options(GPIO_BANK_LED, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ, GPIO_LED1 | GPIO_LED2);
-  gpio_mode_setup(GPIO_BANK_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_LED1 | GPIO_LED2);
-
-  /* In case of EMI */
-  gpio_port_config_lock(GPIO_BANK_LED, GPIO_LED1 | GPIO_LED2);
-}
-
-/* Interrupts */
-
-static void halt_normal(void) {
-  while (true);
-}
-
+/* Exceptions */
 static void halt(void) {
   uint32_t i;
 
@@ -164,8 +47,7 @@ static void halt(void) {
     for (i = 0; i < 10000000; i ++) {
       asm("nop\n");
     }
-    gpio_toggle(GPIO_BANK_LED, GPIO_LED1);
-    gpio_toggle(GPIO_BANK_LED, GPIO_LED2);
+    gpio_toggle(GPIO_PORT_LED, GPIO_PIN_LED1 | GPIO_PIN_LED2);
   }
 }
 
@@ -176,6 +58,129 @@ void hard_fault_handler(void)
 __attribute__ ((alias ("halt")));
 
 
+/* ADC */
+static void setup_adc(void) {
+  rcc_periph_clock_enable(RCC_ADC1);
+
+  gpio_mode_setup(GPIO_PORT_CS, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO_PIN_CSP | GPIO_PIN_CSN);
+
+  adc_power_off(ADC1);
+  adc_set_clk_source(ADC1, ADC_CLKSOURCE_PCLK_DIV4); /* Set ADC clock to 12 MHz, see also ADC_CLOCK */
+  adc_calibrate(ADC1);
+  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_239DOT5); /* (239.5 + 1.5 ~= 50 ksps), see also ADC_CONV_CYCS */
+  adc_set_resolution(ADC1, ADC_RESOLUTION_12BIT); /* See also ADC_DIGITS */
+  adc_set_right_aligned(ADC1);
+  adc_power_on(ADC1);
+
+  adc_set_single_conversion_mode(ADC1);
+}
+
+static uint32_t read_adc_ma(void) {
+  unsigned i;
+  uint32_t acc_csp = 0;
+  uint32_t acc_csn = 0;
+  uint32_t acc_cs;
+  uint32_t ma;
+  /* const but prefer to store in SRAM */
+  uint8_t ch_csp = ADC_CH_CSP;
+  uint8_t ch_csn = ADC_CH_CSN;
+
+  /* Oversampling act as FIR LPF */
+  for (i = 0; i < ADC_OVERSAMP; i ++) {
+    adc_set_regular_sequence(ADC1, 1, &ch_csp);
+    adc_start_conversion_regular(ADC1);
+    while (!adc_eoc(ADC1));
+    acc_csp += adc_read_regular(ADC1);
+    //
+    adc_set_regular_sequence(ADC1, 1, &ch_csn);
+    adc_start_conversion_regular(ADC1);
+    while (!adc_eoc(ADC1));
+    acc_csn += adc_read_regular(ADC1);
+  }
+
+  /* mV per LSB = ADC_VDDA_MV / ADC_DIGITS, mA per mV = 1000 / ADC_R_MOHM, mA per LSB = ADC_VDDA_MV * 1000 / ADC_DIGITS / ADC_R_MOHM (approx 8 mA in current setup) */
+  acc_cs = acc_csp - acc_csn;
+  ma = (acc_cs * ADC_VDDA_MV * 1000) / (ADC_DIGITS * ADC_R_MOHM * ADC_OVERSAMP);
+
+  return ma;
+}
+
+/* PWM */
+/* Complementary PWM with deadtime using GP Timer (instead of Advanced TIM1) */
+/* Implemented with center-aligned counter and 2 PWM channels in different modes */
+/* NOTE: this further limits freq resolution... */
+
+static void disable_pwm(void) {
+  /* Must be in this sequence to avoid shoot-through */
+  timer_set_oc_mode(PWM_TIM, PWM_OC_HI, TIM_OCM_INACTIVE);
+  timer_set_oc_mode(PWM_TIM, PWM_OC_LO, TIM_OCM_ACTIVE);
+}
+
+static void enable_pwm(void) {
+  /* Must be in this sequence to avoid shoot-through */
+  timer_set_oc_mode(PWM_TIM, PWM_OC_LO, TIM_OCM_PWM2);
+  timer_set_oc_mode(PWM_TIM, PWM_OC_HI, TIM_OCM_PWM1);
+}
+
+//TODO: sync (use ETR to reset counter...)
+static void setup_pwm(void) {
+  gpio_set_output_options(GPIO_PORT_PWM, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO_PIN_HI | GPIO_PIN_LO);
+  gpio_set_af(GPIO_PORT_PWM, GPIO_AF2, GPIO_PIN_SYNC | GPIO_PIN_HI | GPIO_PIN_LO);
+  gpio_mode_setup(GPIO_PORT_PWM, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_HI | GPIO_PIN_LO);
+  gpio_mode_setup(GPIO_PORT_PWM, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, GPIO_PIN_SYNC);
+
+  rcc_periph_clock_enable(PWM_TIM_RCC);
+  rcc_periph_reset_pulse(PWM_TIM_RST);
+
+  timer_disable_oc_output(PWM_TIM, TIM_OC1);
+  timer_disable_oc_output(PWM_TIM, TIM_OC2);
+  timer_disable_oc_output(PWM_TIM, TIM_OC3);
+  timer_disable_oc_output(PWM_TIM, TIM_OC4);
+
+  timer_set_mode(PWM_TIM, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_CENTER_1, TIM_CR1_DIR_UP);
+  timer_set_prescaler(PWM_TIM, 0);
+  timer_continuous_mode(PWM_TIM);
+
+  timer_disable_oc_clear(PWM_TIM, PWM_OC_HI);
+  timer_enable_oc_preload(PWM_TIM, PWM_OC_HI);
+  timer_set_oc_mode(PWM_TIM, PWM_OC_HI, TIM_OCM_PWM1); /* Active when TIMx_CNT < TIMx_CCR1 */
+  timer_disable_oc_clear(PWM_TIM, PWM_OC_LO);
+  timer_enable_oc_preload(PWM_TIM, PWM_OC_LO);
+  timer_set_oc_mode(PWM_TIM, PWM_OC_LO, TIM_OCM_PWM2); /* Active when TIMx_CNT >= TIMx_CCR1 */
+
+  disable_pwm();
+  /* Output should always be active (otherwise GPIO becomes high-Z) */
+  timer_enable_counter(PWM_TIM);
+  timer_enable_oc_output(PWM_TIM, PWM_OC_LO);
+  timer_enable_oc_output(PWM_TIM, PWM_OC_HI);
+}
+
+/* freq in Hz, duty/deadtime in 0 - 255 */
+static void config_pwm(uint32_t freq, uint8_t duty, uint8_t deadtime) {
+  uint16_t period = rcc_apb1_frequency / freq / 2 - 1;
+  uint16_t ocv_hi = (((uint32_t)duty) * ((uint32_t)period)) / 255 + 1;
+  uint16_t ocv_lo = ocv_hi + (((uint32_t)deadtime) * ((uint32_t)period)) / 255;
+
+  if (duty + deadtime >= 255) {
+    /* Error: duty/deadtime too large */
+    halt();
+  }
+
+  if (ocv_lo <= ocv_hi) {
+    /* Error: deadtime too small */
+    halt();
+  }
+
+  disable_pwm(); /* Avoid shoot-through */
+
+  timer_set_period(PWM_TIM, period);
+  timer_set_counter(PWM_TIM, 0);
+  timer_set_oc_value(PWM_TIM, PWM_OC_HI, ocv_hi);
+  timer_set_oc_value(PWM_TIM, PWM_OC_LO, ocv_lo);
+
+  enable_pwm();
+}
+
 static void delay_a_bit(void) {
   uint32_t i;
 
@@ -184,80 +189,106 @@ static void delay_a_bit(void) {
   }
 }
 
-#define DEAD_TIME 0 /* Controlled by hardware for L6491 */
+static void setup_hci(void) {
+  gpio_mode_setup(GPIO_PORT_BUT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_PIN_BUT);
+
+  gpio_set_output_options(GPIO_PORT_LED, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ, GPIO_PIN_LED1 | GPIO_PIN_LED2);
+  gpio_mode_setup(GPIO_PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_LED1 | GPIO_PIN_LED2);
+  gpio_set(GPIO_PORT_LED, GPIO_PIN_LED1 | GPIO_PIN_LED2);
+  delay_a_bit();
+  gpio_clear(GPIO_PORT_LED, GPIO_PIN_LED1 | GPIO_PIN_LED2);
+}
+
+static void wait_button(void) {
+  uint32_t i = 0;
+
+  while (!gpio_get(GPIO_PORT_BUT, GPIO_PIN_BUT)) {
+    i ++;
+    if (((i % 500000) == 1) || ((i % 500000) == 10000)) {
+      gpio_toggle(GPIO_PORT_LED, GPIO_PIN_LED1 | GPIO_PIN_LED2);
+    }
+  }
+
+  /* De-bounce */
+  for (i = 0; i < 100000; i ++) {
+    if (gpio_get(GPIO_PORT_BUT, GPIO_PIN_BUT)) {
+      i = 0;
+    }
+  }
+}
+
 #define F_MAX     1200000
 #define F_MIN      800000
 #define F_STEP      20000 /* 1/48MHz ~= 21ns. @ 800kHz, delta_f = 13kHz. @ 1200kHz, delta_f = 29kHz. */
 #define N_FREQ    ((F_MAX - F_MIN) / F_STEP + 1)
+#define DUTY      100 /* out of 255. TODO: Feedback mechanism */
+#define DEAD_TIME 20
 
 #define CURR_LOW  150 /* mA. ~60 noise floor... */
 
-/* FIXME: OCP condition should be determined by pattern rather than absolute value. */
-
-
-/* NOTE: Local minimum won't work due to noise & transient effects */
+// TODO: improve algorithm for hunting optimal work point...
 
 int main(void) {
   uint32_t i;
-  volatile uint32_t freq, i_min, adc_min, i_max_ocp; /* Want debugger access for these */
-  uint32_t adc[N_FREQ];
-  uint32_t curr_ma[N_FREQ]; /* Also for debugger access */
+  /* Want debugger access for these */
+  volatile uint32_t freq, i_min, curr_min, i_max_ocp;
+  volatile uint32_t curr_ma[N_FREQ];
 
   /* Enable GPIOA and GPIOB */
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
-  rcc_periph_clock_enable(RCC_GPIOC);
 
-  led_setup();
+  setup_hci();
 
-  gpio_set(GPIO_BANK_LED, GPIO_LED1);
+  gpio_set(GPIO_PORT_LED, GPIO_PIN_LED1);
   rcc_clock_setup_in_hsi_out_48mhz();
   setup_adc();
-  button_setup();
-  gpio_set(GPIO_BANK_LED, GPIO_LED2);
+  setup_pwm();
+  gpio_set(GPIO_PORT_LED, GPIO_PIN_LED2);
+
+  /* In case of EMI (ADC setup already done) */
+  gpio_port_config_lock(GPIOA, GPIO_ALL);
+  gpio_port_config_lock(GPIOB, GPIO_ALL);
 
   while (true) {
-    button_wait();
-    gpio_clear(GPIO_BANK_LED, GPIO_LED1);
+    wait_button();
+    gpio_clear(GPIO_PORT_LED, GPIO_PIN_LED1);
 
     for (i = 0; i < N_FREQ; i ++) {
       freq = F_MIN + i * F_STEP;
       delay_a_bit();
-      swcap_setup(freq, 50, DEAD_TIME);
+      config_pwm(freq, DUTY, DEAD_TIME);
       delay_a_bit();
-      gpio_set(GPIO_BANK_LED, GPIO_LED1);
-      adc[i] = read_adc();
-      curr_ma[i] = (adc[i] / ADC_OVERSAMPLE) * 300 / 81;
-      gpio_clear(GPIO_BANK_LED, GPIO_LED1);
-      timer_disable_counter(TIM1);
+      gpio_set(GPIO_PORT_LED, GPIO_PIN_LED1);
+      curr_ma[i] = read_adc_ma();
+      gpio_clear(GPIO_PORT_LED, GPIO_PIN_LED1);
+      disable_pwm();
     }
 
-    /* FIXME: assuming lowest freq does not trip OCP */
-    adc_min = adc[0];
+    curr_min = curr_ma[0];
     i_min = 0;
     for (i = 1; i < N_FREQ; i ++) {
       if (curr_ma[i] < CURR_LOW) {
         i_max_ocp = i;
         continue;
       }
-      if (adc[i] < adc_min) {
-        adc_min = adc[i];
+      if (curr_ma[i] < curr_min) {
+        curr_min = curr_ma[i];
         i_min = i;
       }
     }
 
     if (i_max_ocp + 1 < N_FREQ) {
-      // FIXME: should be determined by slope in adc[] or curr_ma[]
+      // FIXME: should be determined by slope in curr_ma[]
       freq = F_MIN + (i_max_ocp + 1) * F_STEP;
     } else {
       // NOTE: fallback. Not actually what would work.
       freq = F_MIN + i_min * F_STEP;
     }
-    swcap_setup(freq, 50, DEAD_TIME);
+    config_pwm(freq, DUTY, DEAD_TIME);
 
-    gpio_set(GPIO_BANK_LED, GPIO_LED1);
+    gpio_set(GPIO_PORT_LED, GPIO_PIN_LED1);
   }
 
-  halt_normal();
   return 0;
 }
